82f786dec767f0b9df1e824d2e92a2806ebebd70
commetron <softechstar.pdf@outlook.com>
2022-11-20 14:06:23 -0500

    tissue in trash lol

--- lib/main.dart
+++ lib/main.dart
@@ -308,3 +308,17 @@ class Tissue {
   render(Canvas c) => tissueSprite.renderRect(c, rect);
   update(double t) => rect = isAway ? rect.shift(Offset.infinite) : tissueBox.initialRect;
 }
+
+
+class TissueAway extends Tissue {
+  TissueAway(GameTable game, TissueBox tissueBox) : super(game, tissueBox);
+  render(Canvas c) => tissueSprite.renderRect(c, rect);
+  update(double t) {
+    var speed = 500 * t;
+    Offset target = tissueBox.getTissueUpPosition - Offset(rect.left, rect.top);
+    if (speed < target.distance)
+      rect = rect.shift(Offset.fromDirection(target.direction, speed));
+    else
+      isAway = true;
+  }
+}
